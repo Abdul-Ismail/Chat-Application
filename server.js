7db81b29fb1afdfebbe4e3fe8114408c3a955dcd
Abdul-Ismail <abduul.ismail@gmail.com>
2018-11-19 17:11:09 +0000

    route change

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express')
 const path = require('path');
 
 var app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    // res.send('hello')
 });
 
 app.listen(port)
